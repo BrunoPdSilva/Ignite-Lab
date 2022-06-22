abd89f207db0da909e3efea8fa7d70a092ad9109
Bruno Peres <bruno_16515@hotmail.com>
2022-06-22 10:26:37 -0300

    Dia 02 Ignite Lab concluído

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,11 @@
-import { useEffect } from "react";
-import { gql, useQuery } from "@apollo/client";
-import { client } from "./lib/apollo";
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-   }
-`
-
-interface Lesson {
-  id: string;
-  title: string;
-}
+//PAGES
+import { Event } from "./pages/Event"
 
 function App() {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
-  console.log(data)
-
-  return (
-    <ul>
-      {data?.lessons.map(lesson => {
-        return <li key={lesson.id}>{lesson.title}</li>
-      })}
-    </ul>
+  return(
+    <div>
+      <Event />
+    </div>
   )
 }
 
---
+++ src/pages/Event.tsx
@@ -0,0 +1,15 @@
+import { Header } from "../components/Header"
+import { Video } from "../components/Video"
+import { Sidebar } from "../components/Sidebar"
+
+export function Event() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex flex-1">
+        <Video />
+        <Sidebar />
+      </main>
+    </div>
+  )
+}
