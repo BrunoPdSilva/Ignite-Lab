4e0dd48fb233f4d3be91a3753e8e61410c4a1e94
Bruno Peres <bruno_16515@hotmail.com>
2022-06-23 14:39:04 -0300

    Dia 03 Ignite Lab (Problema com a API)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-//PAGES
-import { Event } from "./pages/Event"
+import { ApolloProvider } from "@apollo/client"
+import { BrowserRouter } from "react-router-dom"
+import { client } from "./lib/apollo"
+import { Router } from "./Router"
 
 function App() {
-  return(
+  return (
     <div>
-      <Event />
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </ApolloProvider>
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { ApolloProvider } from '@apollo/client';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
@@ -8,8 +7,6 @@ import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
       <App />
-    </ApolloProvider>
   </React.StrictMode>
 )
